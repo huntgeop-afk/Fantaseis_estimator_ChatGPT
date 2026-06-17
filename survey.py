4c9e082ae02d0be85e15907fa78dcf9219a826da
huntgeop-afk <huntgeop@gmail.com>
2026-06-17 15:39:10 -0400

    Update survey.py

--- survey.py
+++ survey.py
@@ -1,1 +1,16 @@
+from dataclasses import dataclass
 
+@dataclass
+class Survey:
+    width: float
+    height: float
+
+    receiver_interval: float
+    receiver_line_spacing: float
+    receiver_lines_active: int
+    receiver_lines_spare: int
+
+    source_interval: float
+    source_line_spacing: float
+
+    target_depth: float
